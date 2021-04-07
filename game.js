bbf6a35dff3eade66aa5b82aec3d3fb60335103f
Peter Mace <peterjmace@gmail.com>
2021-04-07 14:15:22 -0700

    starting a new game

---
+++ game.js
@@ -0,0 +1,11 @@
+class Game{
+    constructor(playerOne, playerTwo){
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo
+    }
+    getName(){
+        return `${this.playerOne} vs. ${this.playerTwo}`;
+    }
+}
+
+export default Game;
