39b619a14fb86d71fcde07b644e9103c50f88d10
username <mantaoxu2@gmail.com>
2021-04-07 14:32:47 -0700

    some

--- connect-four.js
+++ connect-four.js
@@ -1,4 +1,20 @@
 import Game from './game.js'
 
-let game = new Game ('Peter', 'Doris');
-console.log(game);
+// let game = new Game ('Peter', 'Doris');
+// console.log(game);
+let game = undefined;
+
+window.addEventListener("DOMContentLoaded", event => {
+  let playOneInput = document.getElementById('player-1-name');
+  let playTwoInput = document.getElementById('player-2-name');
+  let newGameButton = document.getElementById('new-game');
+  playOneInput.addEventListener('keyup', event => {
+    newGameButton.removeAttribute('disabled');
+
+  })
+  playTwoInput.addEventListener('keyup', event => {
+    newGameButton.removeAttribute('disabled');
+
+  })
+
+})
