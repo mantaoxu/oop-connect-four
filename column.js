58e5c2b6f5b33983becf9fdaef78af126c115b6c
Peter Mace <peterjmace@gmail.com>
2021-04-08 12:48:44 -0700

    getTokenAt added to column class

--- column.js
+++ column.js
@@ -1,16 +1,38 @@
 export default class Column {
   constructor(){
-    this.tokens = [null, null, null, null, null, null];
+    this.tokens = [];
+    //6 undefined
 
   }
   add(currentPlayer){
     let bottomMost = -1;
-    for (let i = 0; i < this.tokens.length; i++){
+    for (let i = 6; i > 0; i--){
       let token = this.tokens[i];
-      if (token === null){
+      //debugger;
+      if (token === undefined){
         bottomMost = i;
       }
     }
-    this.tokens[bottomMost] = currentPlayer;
+    if(bottomMost > -1){
+      this.tokens[bottomMost] = currentPlayer;
+    }
   }
+
+  getTokenAt(rowIndex){
+    //first pull the token at the specified rowIndex
+    //check if undefined
+    //return either null or the player's token at that index
+    let token = this.tokens[rowIndex];
+
+      //game.getTokenAt(5,2)
+      //const column = game.getColumn(2)
+      //column. getTokenAt(5)
+
+    if(token === undefined){
+      return null;
+    }else{
+      return token;
+    }
+  }
+  
 }
