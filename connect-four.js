12350deaa2be9cff7a0ec4456f16ef134d420b3a
username <mantaoxu2@gmail.com>
2021-04-08 07:59:00 -0700

    used format document

--- connect-four.js
+++ connect-four.js
@@ -1,64 +1,58 @@
-import Game from './game.js'
+import Game from "./game.js";
 
 // let game = new Game ('Peter', 'Doris');
 // console.log(game);
 let game = undefined;
 
-function updateUI(){
-  let board = document.getElementById('board-holder');
-  if(game !== undefined) {
-    board.classList.remove('is-invisible');
-    let gameName = document.getElementById('game-name');
+function updateUI() {
+  let board = document.getElementById("board-holder");
+  if (game !== undefined) {
+    board.classList.remove("is-invisible");
+    let gameName = document.getElementById("game-name");
     gameName.innerHTML = game.getName();
+  } else {
+    board.classList.add("is-invisible");
   }
-  else {
-    board.classList.add('is-invisible');
-  }
-};
+}
 
-window.addEventListener("DOMContentLoaded", event => {
+window.addEventListener("DOMContentLoaded", (event) => {
+  let playOneInput = document.getElementById("player-1-name");
+  let playTwoInput = document.getElementById("player-2-name");
+  let newGameButton = document.getElementById("new-game");
 
-  let playOneInput = document.getElementById('player-1-name');
-  let playTwoInput = document.getElementById('player-2-name');
-  let newGameButton = document.getElementById('new-game');
-
-  let targets = document.getElementById('click-targets');
-  targets.addEventListener('click', event=>{
-    game.playInColumn()
+  let targets = document.getElementById("click-targets");
+  targets.addEventListener("click", (event) => {
+    game.playInColumn();
     updateUI();
-  })
+  });
 
+  function checkForBothPlayers() {
+    //checks if player one and play two input is null
+    let isPlayerOne = playOneInput.value.length > 0;
+    let isPlayerTwo = playTwoInput.value.length > 0;
 
-
-    function checkForBothPlayers(){
-        //checks if player one and play two input is null
-        let isPlayerOne = playOneInput.value.length > 0
-        let isPlayerTwo = playTwoInput.value.length > 0
-
-        //if both player one and two have a name
-        if (isPlayerOne  &&  isPlayerTwo){
-            //enable new game
-            newGameButton.disabled = false;//'('disabled');
-        }else{
-            newGameButton.disabled = true;
-        }
-    }
-
-  playOneInput.addEventListener('keyup', event => {
-    checkForBothPlayers()
-  })
-
-  playTwoInput.addEventListener('keyup', event => {
-    checkForBothPlayers()
-  })
-  
-  newGameButton.addEventListener('click', event=>{
-      game = new Game(playOneInput.value, playTwoInput.value);
-      playOneInput.value = '';
-      playTwoInput.value = '';
+    //if both player one and two have a name
+    if (isPlayerOne && isPlayerTwo) {
+      //enable new game
+      newGameButton.disabled = false; //'('disabled');
+    } else {
       newGameButton.disabled = true;
-      updateUI();
+    }
+  }
 
-  })
+  playOneInput.addEventListener("keyup", (event) => {
+    checkForBothPlayers();
+  });
 
-})
+  playTwoInput.addEventListener("keyup", (event) => {
+    checkForBothPlayers();
+  });
+
+  newGameButton.addEventListener("click", (event) => {
+    game = new Game(playOneInput.value, playTwoInput.value);
+    playOneInput.value = "";
+    playTwoInput.value = "";
+    newGameButton.disabled = true;
+    updateUI();
+  });
+});
